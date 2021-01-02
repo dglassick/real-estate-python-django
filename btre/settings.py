dc9cb4b19267bcca5b2290b5db21e0bf117778bb
Derek Glassick <derek.glassick@gmail.com>
2021-01-02 13:55:38 -0800

    added the ability for user to make inquiries, user dashboard dynamically generates with the inquiries that they have submitted, code to send email is commented out so that the client could add their own information there. Application is fully functional, just needs to be deployed.

--- btre/settings.py
+++ btre/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
     'accounts.apps.AccountsConfig',
+    'contacts.apps.ContactsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -140,3 +141,11 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS ={
     messages.ERROR: 'danger',
 }
+
+# Email config
+
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = 'gmail here'
+# EMAIL_HOST_PASSWORD = 'password here'
+# EMAIL_USE_TLS=True
